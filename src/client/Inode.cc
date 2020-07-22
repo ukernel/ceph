ideepika/ceph-ci

client: check inode type for CEPH_CAP_FILE_{CACHE,RD,REXTEND,LAZYIO}
they will have new meaning for directory inode

Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/client/Inode.cc
@@ -305,7 +305,7 @@ int Inode::caps_file_wanted()
 int Inode::caps_wanted()
 {
   int want = caps_file_wanted() | caps_used();
-  if (want & CEPH_CAP_FILE_BUFFER)
+  if (is_file() && (want & CEPH_CAP_FILE_BUFFER))
     want |= CEPH_CAP_FILE_EXCL;
   return want;
 }

ukernel/ceph

src/client/Inode.cc
@@ -415,7 +415,7 @@ int Inode::caps_wanted()
 {
   int want = caps_file_wanted();
   int used = caps_used();
-  if (used & CEPH_CAP_FILE_BUFFER)
+  if (is_file() && (used & CEPH_CAP_FILE_BUFFER))
     want |= CEPH_CAP_FILE_EXCL;
   return want;
 }